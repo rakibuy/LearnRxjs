b9b26bc69b287b3891fe28f1f99607bd1e76279c
Rakibul Islam <rakibul.islam@NEXT_IT.com>
2023-01-08 17:11:34 +0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PromiseComponent } from './includes/promise/promise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'promis', component:PromiseComponent},
+  {path:'', component:PromiseComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './includes/headers/headers.component';
+import { PromiseComponent } from './includes/promise/promise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    PromiseComponent
   ],
   imports: [
     BrowserModule,
